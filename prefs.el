163b8a5e212476e005aeaefdbeb1a9e6f47cd89d
Przemyslaw Kryger <pkryger@gmail.com>
2015-06-12 12:24:01 +0100

    use DejaVu font and decrease frame size

--- prefs.el
+++ prefs.el
@@ -1,3 +1,6 @@
 ;; Default Emacs frame size in chars
 (setq exordium-preferred-frame-width  100
-      exordium-preferred-frame-height 49)
+      exordium-preferred-frame-height 45)
+(setq exordium-preferred-fonts
+      '(("DejaVu Sans Mono for Powerline" . 140)
+        ("Monaco" . 140)))
