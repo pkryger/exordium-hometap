860a8daabbcd7d4eb2b7ad5ec7fc717bb48f2aab
Przemyslaw Kryger <pkryger@gmail.com>
2016-08-20 11:44:44 +0100

    Change font to Hack

--- prefs.el
+++ prefs.el
@@ -2,5 +2,6 @@
 (setq exordium-preferred-frame-width  100
       exordium-preferred-frame-height 45)
 (setq exordium-preferred-fonts
-      '(("DejaVu Sans Mono for Powerline" . 140)
-        ("Monaco" . 140)))
+      '(("Hack Regular" . 120)
+        ("DejaVu Sans Mono for Powerline" . 120)
+        ("Monaco" . 120)))
