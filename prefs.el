cfaede914349e70bff2dd842535af6929ea8cb6c
Przemyslaw Kryger <pkryger@gmail.com>
2018-03-05 17:43:35 +0000

    Change font and turn on rtags

--- prefs.el
+++ prefs.el
@@ -2,8 +2,11 @@
 (setq exordium-preferred-frame-width  100
       exordium-preferred-frame-height 45)
 (setq exordium-preferred-fonts
-      '(("Hack Regular" . 120)
+      '(("Hack" . 120)
         ("DejaVu Sans Mono for Powerline" . 120)
+        ("M+ 1mn" . 120)
+        ("M+ 1m" . 120)
         ("Monaco" . 120)))
 
-(setq exordium-rtags-cmake-build-dir "bld")
+(setq exordium-rtags-cmake-build-dir "../bld")
+(setq exordium-rtags-cmake t)
