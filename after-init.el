b220743c2bc7f4f0c789d97b17a25c9a2497809d
Przemyslaw Kryger <pkryger@gmail.com>
2020-08-29 10:45:56 +0100

    Don't use locally installed cmake nor brew lisp

--- after-init.el
+++
@@ -1,3 +0,0 @@
-;; Add CMAKE mode
-(setq load-path (cons (expand-file-name "/usr/local/share/cmake/editors/emacs/") load-path))
-(require 'cmake-mode)
--- before-init.el
+++
@@ -1,2 +0,0 @@
-(add-to-list 'load-path "/usr/local/share/emacs/site-lisp")
-(add-to-list 'load-path "/usr/local/share/emacs/site-lisp/cmake")
