5108f669106b09d310041e8116d6b17dd33f5591
Przemyslaw Kryger <pkryger@gmail.com>
2022-12-05 18:08:08 +0000

    fira-code

--- prefs.el
+++ prefs.el
@@ -11,3 +11,26 @@
 
 (setq exordium-rtags-cmake-build-dir "../bld")
 (setq exordium-rtags-cmake t)
+
+(defcustom exordium-preferred-variable-fonts
+  '(("Fira Sans"  . 125))
+  ;; TODO: come up with a list of reasonable fonts
+  "TODO: List of preferred fonts/sizes to use, in decreasing order of
+preference. We will use the first one that is available on the
+local machine. It is a list of pairs (font-name . font-size). If
+nil, we don't set any font. Note that you can get the list of
+available font names by evaluating (font-family-list)."
+  ;; TODO::group 'exordium
+  :type  'sexp)
+
+(setq exordium-preferred-variable-fonts
+      '(("Fira Sans" . 125)))
+
+;; (custom-set-faces '(variable-pitch ((t (:family "Fira Sans" :height 135)))))
+
+;; (set-face-attribute 'default nil
+;;                     :family font
+;;                     :height size
+;;                     :weight 'normal)
+
+;;
